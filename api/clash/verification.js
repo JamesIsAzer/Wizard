3f486331e124a078c21f14c11d87530f18beb579
James <29158731+JamesIsAzer@users.noreply.github.com>
2022-07-31 21:08:53 +1000

    API layer design for clash

---
+++ api/clash/verification.js
@@ -0,0 +1,28 @@
+const {clashHeader} = require('../../utils/headers')
+const axios = require('axios').default;
+const {parseClashStatus} = require('../../utils/statusLogger')
+
+const responseObject = (response, fallback) => ({
+  response: response,
+  error: fallback
+})
+
+const verifyProfileRequest = async ({ id, token }) => {
+    return axios.post(
+      `https://api.clashofclans.com/v1/players/%23${id.toUpperCase()}/verifytoken`, 
+      { token: token },
+      clashHeader
+    ).then((response) => { return response })
+    .catch((error) => { return error.response })
+};
+
+const verifyProfile = async( id, token ) => {
+  const response = await verifyProfileRequest({id, token})
+  if (response.status === 200) return responseObject(response.data, null)
+  return responseObject(null, parseClashStatus(clashStatus))
+}
+
+module.exports = {
+  verifyProfile
+}
+
