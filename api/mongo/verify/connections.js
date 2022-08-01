5b84e046bcebe25f706c3687a90e442bd1920a6b
Hawk Eye <hawkeyesalt@gmail.com>
2022-08-01 15:10:25 +0530

    Added refresh command, moved common functionality to new file

--- api/mongo/verify/connections.js
+++ api/mongo/verify/connections.js
@@ -1,45 +1,64 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const verify = require('./schema')
+const verify = require('./schema');
 
 const tagVerified = async (tag) => {
-    return verify.findOne({
-        playerTag: tag
-    }).then((result) => {
-        if (result) return true
-        return false
+  return verify
+    .findOne({
+      playerTag: tag,
     })
-}
+    .then((result) => {
+      if (result) return true;
+      return false;
+    });
+};
 
 const alreadyTaken = async (tag, discordID) => {
-    return verify.findOne({
-        discordID: { $ne: discordID },
-        playerTag: tag
-    }).then((result) => {
-        if(result) return true
-        return false
+  return verify
+    .findOne({
+      discordID: { $ne: discordID },
+      playerTag: tag,
     })
-}
+    .then((result) => {
+      if (result) return true;
+      return false;
+    });
+};
+
+const tagVerifiedBySameUser = async (tag, discordID) => {
+  return verify
+    .findOne({
+      discordID,
+      playerTag: tag,
+    })
+    .then((result) => {
+      if (result) return true;
+      return false;
+    });
+};
 
 const insertVerification = async (tag, discordID) => {
-    return verify.create({
-        discordID: discordID,
-        playerTag: tag,
-        leaderboard: false,
-        trophies: 0,
-        builderleaderboard: false,
-        buildertrophies: 0
-    }).catch((e) => console.log(e))
-}
+  return verify
+    .create({
+      discordID: discordID,
+      playerTag: tag,
+      leaderboard: false,
+      trophies: 0,
+      builderleaderboard: false,
+      buildertrophies: 0,
+    })
+    .catch((e) => console.log(e));
+};
 
 module.exports = {
-    tagVerified,
-    alreadyTaken,
-    insertVerification
-}
+  tagVerified,
+  alreadyTaken,
+  insertVerification,
+  tagVerifiedBySameUser,
+};
