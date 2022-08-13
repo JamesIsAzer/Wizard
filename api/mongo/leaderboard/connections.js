428051788a50f19b48a9aea2612546d09c3ead83
James <29158731+JamesIsAzer@users.noreply.github.com>
2022-08-13 14:29:20 +1000

    Permission handler with lock leaderboard command

---
+++ api/mongo/leaderboard/connections.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const guildToggles = require('./schema');
+
+const { IDs } = require('../../../config.json');
+
+const toggleLeaderboard = async (lockLeaderboard) => 
+    guildToggles.updateOne({
+        guildID: IDs.guild
+    }, 
+    { $set: { lockLeaderboard: lockLeaderboard } },
+    { upsert: true })
+
+module.exports = {
+    toggleLeaderboard
+}
---
+++ service/commands/leaderboards/lockleaderboard.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { hasMediumPerms } = require('../../../utils/permissions');
+const {
+  toggleLeaderboard,
+} = require('../../../api/mongo/leaderboard/connections');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('lockleaderboard')
+    .setDescription('Enable/disable people from participating on the leaderboard')
+    .addBooleanOption((option) =>
+      option
+        .setName('lock')
+        .setDescription('Should the leaderboard be locked')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    if(hasMediumPerms(interaction.member)){
+      const shouldLock = interaction.options.getBoolean('lock');
+      toggleLeaderboard(shouldLock)
+      await interaction.editReply(`Leaderboard ${shouldLock ? 'locked' : 'unlocked'}`)        
+    }
+    else await interaction.editReply(`Insufficient permissions to use this command.`)
+  }
+};
