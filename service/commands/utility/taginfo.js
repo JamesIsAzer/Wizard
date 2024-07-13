6d66cdb707ce006a911798e05830857935aeec9e
Hawk Eye <hawkeyesalt@gmail.com>
2024-07-13 18:58:52 +0530

    Addressing comments

--- service/commands/utility/taginfo.js
+++ service/commands/utility/taginfo.js
@@ -2,11 +2,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const {
   getDiscordOfTag,
 } = require('../../../dao/mongo/verification/connections');
-const {
-  parseTag,
-  isTagValid,
-} = require('../../../utils/arguments/tagHandling');
-const { getInvalidTagEmbed } = require('../../../utils/embeds/verify');
+const { parseTag } = require('../../../utils/arguments/tagHandling');
 const { hasMediumPerms } = require('../../../utils/permissions');
 
 module.exports = {
@@ -25,22 +21,14 @@ module.exports = {
     const targetPlayerTag = parseTag(
       interaction.options.getString('playertag')
     );
-    if (!isTagValid(targetPlayerTag)) {
-      await interaction.reply({
-        embeds: [getInvalidTagEmbed()],
-        ephemeral: true,
-      });
-      return;
-    }
-    const tagVerification = await getDiscordOfTag(targetPlayerTag);
-    if (!tagVerification) {
-      interaction.reply({
+    const playersDiscordID = await getDiscordOfTag(targetPlayerTag);
+    if (!playersDiscordID) {
+      return interaction.reply({
         content: `No verifications found for tag ${targetPlayerTag}.`,
         ephemeral: false,
       });
-      return;
     }
 
-    interaction.reply(tagVerification);
+    interaction.reply(playersDiscordID);
   },
 };
