d5cb46a0af862b09134d47f8314eb3213d72df90
James <29158731+JamesIsAzer@users.noreply.github.com>
2025-06-13 01:32:11 +1000

    Shortening load message

--- utils/embeds/loading.js
+++ utils/embeds/loading.js
@@ -2,7 +2,7 @@ const { EmbedBuilder } = require('discord.js')
 const { loading, hairflip } = require('../../emojis.json')
 
 const getLoadingEmbed = () => new EmbedBuilder()
-    .setDescription(`${loading} Generating info, in the meantime enjoy this hairflip ${hairflip}`)
+    .setDescription(`${loading} Generating info, please enjoy this hairflip ${hairflip}`)
 
 module.exports = {
     getLoadingEmbed
