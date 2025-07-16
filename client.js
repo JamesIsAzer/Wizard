5e388f9c1cfda72bb5e2428f5dfafd85f04cba77
James <29158731+JamesIsAzer@users.noreply.github.com>
2025-07-17 01:07:03 +1000

    Removing a now unused intent

--- client.js
+++ client.js
@@ -3,8 +3,7 @@ const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds, 
-    GatewayIntentBits.GuildMessages, 
-    GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMessages
   ],
   partials: [Partials.Channel]
 });
