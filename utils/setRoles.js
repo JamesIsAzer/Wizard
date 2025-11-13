8a048c920a591e04b719d17a30504f0869ed82c9
Hawk Eye <hawkeyesalt@gmail.com>
2025-11-13 17:39:14 +0530

    Revert "Merge pull request #7 from JamesIsAzer/bugfix-adding-legacy-legends-role"
    This reverts commit c72e2dd538860d564b3a9b620d12f488040c5668, reversing
    changes made to 34c04827e1e9ce7b84c322ac95ee2bede5cb8bb3.

--- utils/setRoles.js
+++ utils/setRoles.js
@@ -69,7 +69,6 @@ const addAchievementRoles = (user, achieved, verificationRoles) => {
   }
 
   addAchievementRole(achieved.legends, verificationRoles?.legends)
-  addAchievementRole(achieved.legendsOld, verificationRoles?.legendsOld)
   addAchievementRole(achieved.starLord, verificationRoles?.starLord)
   addAchievementRole(achieved.farmersRUs, verificationRoles?.farmersRUs)
   addAchievementRole(achieved.masterBuilder, verificationRoles?.masterBuilder)
